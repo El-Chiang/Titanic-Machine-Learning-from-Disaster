66da659991274b4f9d1396f407fdedd151004c00
junyu Jiang <jing1downey@gmail.com>
2017-12-03 21:48:20 +0800

    逻辑回归建模

--- titanic_model.py
+++ titanic_model.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pandas as pd
-import sklearn.preprocessing as preprocessing
+from sklearn import linear_model, preprocessing
 from sklearn.ensemble import RandomForestRegressor
 
 data_train = pd.read_csv(r"./data/train.csv")  # 读取csv
+
 
 def set_missing_ages(df):
     """使用RandomForestClassifier填补缺失的年龄
@@ -38,6 +40,7 @@ def set_cabin_type(df):
     df.loc[(df.Cabin.isnull()), 'Cabin'] = 'No'
     return df
 
+
 data_train, rfr = set_missing_ages(data_train)
 data_train = set_cabin_type(data_train)
 
@@ -47,12 +50,68 @@ dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix='Embarked')
 dummies_Sex = pd.get_dummies(data_train['Sex'], prefix='Sex')
 dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix='Pclass')
 
-df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
-df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked,
+                dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket',
+         'Cabin', 'Embarked'], axis=1, inplace=True)
 
 # 对Age和Fare属性进行scaling
 scaler = preprocessing.StandardScaler()
 age_scale_param = scaler.fit(df['Age'].values.reshape(-1, 1))
-df['Age_scaled'] = scaler.fit_transform(df['Age'].values.reshape(-1, 1), age_scale_param)
+df['Age_scaled'] = scaler.fit_transform(
+    df['Age'].values.reshape(-1, 1), age_scale_param)
 fare_scale_param = scaler.fit(df['Fare'].values.reshape(-1, 1))
-df['Fare_scaled'] = scaler.fit_transform(df['Fare'].values.reshape(-1, 1), fare_scale_param)
+df['Fare_scaled'] = scaler.fit_transform(
+    df['Fare'].values.reshape(-1, 1), fare_scale_param)
+
+# 逻辑回归建模
+
+# 用正则取出属性值
+train_df = df.filter(
+    regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(X, y)
+
+# 对test.csv做数据预处理
+data_test = pd.read_csv(r"./data/test.csv")
+data_test.loc[(data_test.Fare.isnull()), 'Fare'] = 0
+# 特征变换
+# 先用RandomForestRegressor填补丢失的年龄
+tmp_df = data_test[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[(data_test.Age.isnull()), 'Age'] = predictedAges
+
+data_test = set_cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix='Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix='Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix='Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix='Pclass')
+
+df_test = pd.concat([data_test, dummies_Cabin,
+                     dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket',
+              'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(
+    df_test['Age'].values.reshape(-1, 1), age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(
+    df_test['Fare'].values.reshape(-1, 1), fare_scale_param)
+
+# 获取预测结果
+test = df_test.filter(
+    regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'PassengerId': data_test['PassengerId'].as_matrix(
+), 'Survived': predictions.astype(np.int32)})
+result.to_csv('./data/logistic_regression_predictions.csv', index=False)
